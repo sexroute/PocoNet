9525aab2dc708b91e95da3561c666074bd1541d7
ichenq <ichenq@gmail.com>
2015-01-31 23:16:53 +0800

    remove automatically linkage

--- src/Poco/Foundation.h
+++ src/Poco/Foundation.h
@@ -81,36 +81,6 @@
 
 
 //
-// Automatically link Foundation library.
-//
-#if defined(_MSC_VER)
-	#if defined(POCO_DLL)
-		#if defined(_DEBUG)
-			#define POCO_LIB_SUFFIX "d.lib"
-		#else
-			#define POCO_LIB_SUFFIX ".lib"
-		#endif
-	#elif defined(_DLL)
-		#if defined(_DEBUG)
-			#define POCO_LIB_SUFFIX "mdd.lib"
-		#else
-			#define POCO_LIB_SUFFIX "md.lib"
-		#endif
-	#else
-		#if defined(_DEBUG)
-			#define POCO_LIB_SUFFIX "mtd.lib"
-		#else
-			#define POCO_LIB_SUFFIX "mt.lib"
-		#endif
-	#endif
-
-	#if !defined(POCO_NO_AUTOMATIC_LIBS) && !defined(Foundation_EXPORTS)
-		#pragma comment(lib, "PocoFoundation" POCO_LIB_SUFFIX)
-	#endif
-#endif
-
-
-//
 // Include platform-specific definitions
 //
 #include "Poco/Platform.h"
